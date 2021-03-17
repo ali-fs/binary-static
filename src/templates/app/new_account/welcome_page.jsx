4578b8c3deb62bc2e8e1608997351064e49ac9d3
ali-fs <ali.sabzinezhad@firstsource.tech>
2021-03-17 12:56:07 +0330

    translation fix

--- src/templates/app/new_account/welcome_page.jsx
+++ src/templates/app/new_account/welcome_page.jsx
@@ -1,41 +1,55 @@
 import React from 'react';
 import { SeparatorLine } from '../../_common/components/separator_line.jsx';
 
-const options = [
-    {
-        icon_list    : ['images/pages/welcome/cfd.svg'],
-        title        : 'CFD',
-        desc         : 'Trade with leverage and low spreads for better returns on successful trades.',
-        platform_list: [
-            {
-                icon       : 'metatrader5',
-                title      : 'MetaTrader 5',
-                description: 'Earn fixed payouts with options, or trade multipliers to amplify your gains with limited risk.',
-            },
-        ],
-        action_title: 'Trade on MetaTrader 5',
-        action_id   : 'mt5',
-    },
-    {
-        icon_list    : ['images/pages/welcome/options.svg', 'images/pages/welcome/multipliers.svg'],
-        title        : 'Options and multipliers',
-        desc         : 'Earn fixed payouts with options, or trade multipliers to amplify your gains with limited risk.',
-        platform_list: [
-            {
-                icon       : 'metatrader5',
-                title      : 'SmartTrader',
-                description: 'Trade options with Binary.com\'s legacy trading app.',
-            },
-            {
-                icon       : 'binarybot',
-                title      : 'Binary Bot',
-                description: 'Trade options with Binary.com\'s legacy trading app.',
-            },
-        ],
-        action_title: 'Get started',
-        action_id   : 'default',
-    },
-];
+const WelcomePage = () => {
+    const options = [
+        {
+            icon_list: ['images/pages/welcome/cfd.svg'],
+            title: it.L('CFD'),
+            desc: it.L('Trade with leverage and low spreads for better returns on successful trades.'),
+            platform_list: [
+                {
+                    icon: 'metatrader5',
+                    title: it.L('MetaTrader 5'),
+                    description: it.L('Earn fixed payouts with options, or trade multipliers to amplify your gains with limited risk.'),
+                },
+            ],
+            action_title: it.L('Trade on MetaTrader 5'),
+            action_id: 'mt5',
+        },
+        {
+            icon_list: ['images/pages/welcome/options.svg', 'images/pages/welcome/multipliers.svg'],
+            title: it.L('Options and multipliers'),
+            desc: it.L('Earn fixed payouts with options, or trade multipliers to amplify your gains with limited risk.'),
+            platform_list: [
+                {
+                    icon: 'metatrader5',
+                    title: it.L('SmartTrader'),
+                    description: it.L('Trade options with Binary.com\'s legacy trading app.'),
+                },
+                {
+                    icon: 'binarybot',
+                    title: it.L('Binary Bot'),
+                    description: it.L('Trade options with Binary.com\'s legacy trading app.'),
+                },
+            ],
+            action_title: it.L('Get started'),
+            action_id: 'default',
+        },
+    ];
+
+    return (
+        <div id='welcome_container' className='center-text welcome-content'>
+            <h1>{it.L('Where would you like to start?')}</h1>
+
+            <div className='welcome-content-container'>
+                {options.map(option => (<RenderOption key={option.title} option={option} />))}
+            </div>
+
+            <SeparatorLine className='gr-padding-10' invisible />
+        </div>
+    );
+}
 
 const Platform = ({ title, description, icon }) => (
     <div className='welcome-content-platform-list-container'>
@@ -55,8 +69,8 @@ const RenderOption = ({ option }) => (
                     <img key={icon} className='welcome-content-box-icon' src={it.url_for(icon)} />
                 )}
             </div>
-            <p id='upgrade_text' className='welcome-content-title'>{it.L(option.title)}</p>
-            <p >{it.L(option.desc)}</p>
+            <p id='upgrade_text' className='welcome-content-title'>{option.title}</p>
+            <p >{option.desc}</p>
 
             <SeparatorLine className='gr-padding-5' />
 
@@ -67,23 +81,11 @@ const RenderOption = ({ option }) => (
                 )}
                 <div className='welcome-content-action-container'>
                     <a id={option.action_id} className='button-secondary' href='javascript:;'>
-                        <span className='welcome-content-action'>{it.L(option.action_title)}</span>
+                        <span className='welcome-content-action'>{option.action_title}</span>
                     </a>
                 </div>
             </div>
         </div>
-    </div>
-);
-
-const WelcomePage = () => (
-    <div id='welcome_container' className='center-text welcome-content'>
-        <h1>{it.L('Where would you like to start?')}</h1>
-
-        <div className='welcome-content-container'>
-            {options.map(option => (<RenderOption key={option.title} option={option} />))}
-        </div>
-
-        <SeparatorLine className='gr-padding-10' invisible />
     </div>
 );
 
